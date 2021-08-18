8db851c8822f138c33f94de0907ff2d1c08faaea
Usarneme <jamestcoop@gmail.com>
2021-08-18 09:59:35 -0700

    :tada: adds handler for event hook post route and service to log ot file for testing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('ev_webhook/new_event')
+  @HttpCode(200)
+  async login(@Body() data) {
+    return this.appService.updateFile(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  updateFile(body: string): string {
+    try {
+      fs.writeFile('./log.txt', body, (e) => {
+        if (e) throw e;
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    return 'ok';
   }
 }
